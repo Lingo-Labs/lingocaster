fe556c0da8c5b6fd647e2aa47a5d11c32148d444
MattWong-ca <matthew.yt.wong@gmail.com>
2024-09-09 21:10:50 -0400

    test frames

--- frames/src/index.tsx
+++ frames/src/index.tsx
@@ -1,12 +1,16 @@
 import { serveStatic } from '@hono/node-server/serve-static'
 import { Button, Frog, TextInput } from 'frog'
 import { devtools } from 'frog/dev'
-// import { neynar } from 'frog/hubs'
+import { neynar } from 'frog/hubs'
+import { useState } from 'hono/jsx'
 
 export const app = new Frog({
   // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+  hub: neynar({ apiKey: 'NEYNAR_FROG_FM' }),
+  title: 'Fruit Machine',
 })
+
+// const [home, setHome] = useState('HOME')
 
 app.use('/*', serveStatic({ root: './public' }))
 
@@ -44,9 +48,7 @@ app.frame('/', (c) => {
             whiteSpace: 'pre-wrap',
           }}
         >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+          {'home'}
         </div>
       </div>
     ),
@@ -55,7 +57,58 @@ app.frame('/', (c) => {
       <Button value="apples">Apples</Button>,
       <Button value="oranges">Oranges</Button>,
       <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <Button.Link href="/test">Go to Test Frame</Button.Link>,
+      // status === 'response' && <Button.Reset>Reset</Button.Reset>,
+    ],
+  })
+})
+
+app.frame('/test', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {'test'}
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Enter custom fruit..." />,
+      <Button value="apples">Apples</Button>,
+      <Button value="oranges">Oranges</Button>,
+      <Button value="bananas">Bananas</Button>,
+      <Button.Link href="/">Back to Home</Button.Link>,
+      // <Button.Transaction target="/test">test</Button.Transaction>,
+      // status === 'response' && <Button.Reset>Reset</Button.Reset>,
     ],
   })
 })
