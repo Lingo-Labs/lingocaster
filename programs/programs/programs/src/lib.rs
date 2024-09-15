987e433b8c8129250bcfd8413fb05daf9a0d8c52
MattWong-ca <matthew.yt.wong@gmail.com>
2024-09-14 20:11:28 -0400

    update program

--- programs/programs/programs/src/lib.rs
+++ programs/programs/programs/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
 
 declare_id!("9zRMVhjyPgwyshLT1dBpc7D6unYWbGVFfyanzAGGnxJ4");
 
@@ -20,6 +21,32 @@ pub mod betting {
         ctx.accounts.bet_info.player_one = *ctx.accounts.user_authority.key;
         Ok(())
     }
+
+    pub fn challenge_bet(ctx: Context<ChallengeBet>, amount: u64) -> Result<()> {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.user_token_account.to_account_info(),
+            to: ctx.accounts.vault_token_account.to_account_info(),
+            authority: ctx.accounts.user_authority.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::transfer(cpi_ctx, amount)?;
+
+        ctx.accounts.bet_info.player_two = Some(*ctx.accounts.user_authority.key);
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.vault_token_account.to_account_info(),
+            to: ctx.accounts.winner_token_account.to_account_info(),
+            authority: ctx.accounts.vault_authority.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -33,4 +60,34 @@ pub struct Bet<'info> {
     #[account(mut)]
     pub bet_info: Account<'info, BetInfo>,
     pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct ChallengeBet<'info> {
+    #[account(mut)]
+    pub user_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub vault_token_account: Account<'info, TokenAccount>,
+    #[account(signer)]
+    pub user_authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub bet_info: Account<'info, BetInfo>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub vault_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub winner_token_account: Account<'info, TokenAccount>,
+    pub vault_authority: AccountInfo<'info>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[account]
+pub struct BetInfo {
+    pub bet_amount: u64,
+    pub player_one: Pubkey,
+    pub player_two: Option<Pubkey>,
 }
